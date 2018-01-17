60b48fd2eb8f7824a0aba0db8b5174d07e13a58b
lovick <olsons@scc2102-3.ucdavis.edu>
2018-01-16 16:24:22 -0800

    Moved CSS to a folder, and updated the banner to make (slightly) less ugly.

--- script/banner.js
+++ script/banner.js
@@ -5,12 +5,22 @@ var numBars = 8;
 function setup() {
     var bannerCanv = createCanvas(800, 250);
     bannerCanv.parent('mySketch');
+    textFont("Ubuntu");
 }
 
 function draw() {
     background(color('#39a0ed'));
     for (var i = 0; i < 8; i++) {
+        noStroke();
+        if (i % 2 == 0) {
+            fill('#224870');
+        } else {
+            fill('#4c6085');
+        }
         rect((i*(width/numBars)+offset)%(width+(width/numBars))-100, i*(height/numBars), width/numBars, height/numBars);
     }
+    textSize(70);
+    fill('#f7fff7');
+    text('Sven Olson', 50, 3*height/4);
     offset+=1;
 }
